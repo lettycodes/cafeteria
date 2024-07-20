3589a758b468d5b9913d1e63b1c17cc0e5072a78
Letícia Silva <sps.leticia@gmail.com>
2024-07-20 12:06:04 -0300

    Alterando espaçamento dos badges e formatando preço dos produtos

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,6 +1,10 @@
 import { Badge, Button, Card, Container } from "react-bootstrap";
 import { Link, Navigate } from "react-router-dom";
-import { deleteProduto, getProdutos, getProdutosUsuario } from "../firebase/produtos";
+import {
+  deleteProduto,
+  getProdutos,
+  getProdutosUsuario,
+} from "../firebase/produtos";
 import { useContext, useEffect, useState } from "react";
 import Loader from "../components/Loader";
 import toast from "react-hot-toast";
@@ -50,21 +54,30 @@ function Produtos() {
         {produtos ? (
           <section className="mt-2">
             {produtos.map((produto) => {
+              const precoFormatado = Number(produto.preco).toFixed(2);
+
               return (
                 <Card key={produto.id}>
                   <Card.Body>
                     <Card.Title>{produto.nome}</Card.Title>
                     <Card.Text>{produto.descricao}</Card.Text>
                     <div className="mb-2">
-                      <Badge bg="dark">{produto.categoria}</Badge>
-                      <Badge bg="info">R$ {produto.preco.toFixed(2)}</Badge>
-                      <Badge bg="secondary">Quantidade: {produto.quantidade}</Badge>
+                      <Badge bg="dark" className="me-2">
+                        {produto.categoria}
+                      </Badge>
+                      <Badge bg="info" className="me-2">
+                        R$ {isNaN(precoFormatado) ? "0.00" : precoFormatado}
+                      </Badge>
+                      <Badge bg="secondary" className="me-2">
+                        Quantidade: {produto.quantidade}
+                      </Badge>
                     </div>
                     <Button
                       variant="dark"
                       onClick={() => {
                         navigate(`/produtos/editar/${produto.id}`);
                       }}
+                      className="me-2"
                     >
                       Editar
                     </Button>
