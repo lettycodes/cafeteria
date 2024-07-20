07053865eb90d4ac260804ceae06b1d096fda54b
lettycodes <sps.leticia@gmail.com>
2024-07-20 08:08:05 -0300

    Editando App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Produtos from "./pages/Produtos";
-import RemoverProduto from "./pages/RemoverProduto";
+// import RemoverProduto from "./pages/RemoverProduto";
 import AdicionarProduto from "./pages/AdicionarProduto";
 import EditarProduto from "./pages/EditarProduto";
 import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/produtos" element={<Produtos />} />
-        <Route path="/produtos/adicionar" element={<AdicionarProduto />} />
-        <Route path="/produtos/editar/:id" element={<EditarProduto />} />
-        <Route path="/produtos/remover/:id" element={<RemoverProduto />} />
-      </Routes>
-    </Router>
+    <div className="d-flex flex-column min-vh-100">
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/produtos" element={<Produtos />} />
+          <Route path="/produtos/adicionar" element={<AdicionarProduto />} />
+          <Route path="/produtos/editar/:id" element={<EditarProduto />} />
+          {/* <Route path="/produtos/remover/:id" element={<RemoverProduto />} /> */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
